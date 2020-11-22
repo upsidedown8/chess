a88b1af23435c1383502aaa7fb37231308409967
upsidedown8 <65985223+upsidedown8@users.noreply.github.com>
2020-11-22 12:10:25 +0000

    added display_bitboards function

--- src/main_console.cpp
+++ src/main_console.cpp
@@ -1,11 +1,26 @@
 #include "game/board.hpp"
+#include "game/utils.hpp"
 
 #include <iostream>
 
-int main(int argc, char const *argv[]) {
-    Board board;
+void display_bitboards(chess_cpp::Board &board) {
+    for (int piece = chess_cpp::WP; piece <= chess_cpp::BK; piece++) {
+        std::cout
+            << "bitboard: "
+            << chess_cpp::to_string((chess_cpp::Pieces)piece)
+            << std::endl;
+        std::cout
+            << chess_cpp::to_string(board.m_bitboards[piece])
+            << std::endl;
+    }
+}
 
-    std::cout << board.to_string() << std::endl;
+int main(int argc, char const *argv[]) {
+    chess_cpp::Board board;
+
+    display_bitboards(board);
+    
+    std::cout << board.to_string(true);
 
     return 0;
 }

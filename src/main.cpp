219d57f8763355a7c7d08d254917fcbfc3371c60
Tom Thorogood <tomthorogood@outlook.com>
2021-01-26 00:30:10 +0000

    Call chess_cpp::init() in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,14 @@
 #include "game/board.hpp"
-#include "game/utils.hpp"
 
 #include <iostream>
 
-using namespace chess_cpp;
-
-U64 gen_rook_moves(int start) {
-    int rank = start / 8;
-    int file = start % 8;
-    U64 result = RANKS[rank] | FILES[file];
-    chess_cpp::clr_pos(result, start);
-    return result;
-}
-U64 gen_bishop_moves(int start) {
-    int rank = start / 8;
-    int file = start % 8;
-    
-}
-
 int main(int argc, char const *argv[]) {
-    Board board;
+    chess_cpp::init();
+    // chess_cpp::Board board;
 
-    // U64 pawnsInQuestion = board.m_bitboards[WP] & RANKS[6];
-    // U64 singleMoves = (pawnsInQuestion >> 8) & RANKS[5];
+    // std::cout << board.to_string() << std::endl;
 
-    // std::cout << to_string(pawnsInQuestion) << std::endl;
-    // std::cout << to_string(singleMoves) << std::endl;
+    std::cout << chess_cpp::bb_to_string(chess_cpp::BISHOP_MASKS[chess_cpp::f6]) << std::endl;
 
     return 0;
 }

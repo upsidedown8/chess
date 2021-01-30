f6a01ebc85fad08fd3f73bd44c1986bf43da8b8b
Tom Thorogood <tomthorogood@outlook.com>
2021-01-30 21:44:17 +0000

    Added to_string() and 16 bit storage to move

--- src/game/move.cpp
+++ src/game/move.cpp
@@ -2,12 +2,25 @@
 
 using namespace chess_cpp;
 
-U8 Move::get_start() {
-    return start;
-}
-U8 Move::get_end() {
-    return end;
+Move::Move(int start, int end, U8 flags) {
+    value = (end<<10) | (start<<4) | flags;
 }
 
-Move::Move(U8 start, U8 end)
-    : start(start), end(end) {}
+U8 Move::get_start() {
+    return (value & MOVESTART_FLAG)>>4;
+}
+U8 Move::get_end() {
+    return (value & MOVEEND_FLAG)>>10;
+}
+
+std::string Move::to_string() {
+    std::string result(4, '\0');
+    int r, f;
+    calc_rf(get_start(), r, f);
+    result[0] = 'a'+r;
+    result[1] = '0'+f;
+    calc_rf(get_end(), r, f);
+    result[2] = 'a'+r;
+    result[3] = '0'+f;
+    return result;
+}
--- src/game/move.hpp
+++ src/game/move.hpp
@@ -3,6 +3,15 @@
 
 #include "defs.hpp"
 
+#define MOVEPROMOTION_FLAG  0b0000000000000011
+#define MOVETYPE_FLAG       0b0000000000001100
+#define MOVESTART_FLAG      0b0000001111110000
+#define MOVEEND_FLAG        0b1111110000000000
+
+#define MOVETYPE_CASTLE     0b0000000000000100
+#define MOVETYPE_ENPASSANT  0b0000000000001000
+#define MOVETYPE_PROMOTION  0b0000000000001100
+
 namespace chess_cpp {
 
 /* -------------------------------------------------------------------------- */
@@ -10,18 +19,20 @@ namespace chess_cpp {
 /* -------------------------------------------------------------------------- */
 /**
  * Stored in 16 bits
- * 000000 000000 00              00
- * end    start  promotion piece castling/promotion/en passant flag
+ * 000000  000000  0000
+ * end     start   castling/promotion/en passant flags
  */
 
 class Move {
 public:
-    U8 start, end;
+    U16 value;
+
+    Move(int start, int end, U8 flags);
 
     U8 get_start();
     U8 get_end();
 
-    Move(U8 start, U8 end);
+    std::string to_string();
 };
 
 }

45ecb8f552baf36185851727e41d5aa97b38ebe9
Tom Thorogood <tomthorogood@outlook.com>
2021-01-31 17:37:50 +0000

    Added gameplay test to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,51 @@
 #include "game/board.hpp"
+#include "game/movegen.hpp"
 
 #include <iostream>
 
 using namespace chess_cpp;
 
-U64 calc_rook_moves(int square, U64 occupancy) {
-    occupancy &= ROOK_MASKS[square];
-    int idx = (ROOK_MAGICS[square]*occupancy)>>ROOK_MAGIC_SHIFTS[square]; 
-    return ROOK_MOVES[square][idx];
-}
-U64 calc_bishop_moves(int square, U64 occupancy) {
-    occupancy &= BISHOP_MASKS[square];
-    int idx = (BISHOP_MAGICS[square]*occupancy)>>BISHOP_MAGIC_SHIFTS[square]; 
-    return BISHOP_MOVES[square][idx];
-}
-
 int main(int argc, char const *argv[]) {
     init();
 
-    Board board;
-    std::cout << board.to_string() << std::endl;
+    std::string fen;
+    std::cout << "Enter FEN: ";
+    getline(std::cin, fen);
+    Board board(fen);
+    std::vector<Move> moves;
+
+    while (true) {
+        std::cout << "Board: " << std::endl;
+        std::cout << board.to_string() << std::endl;
+
+        std::cout << "Moves: " << std::endl;
+        U64 attacks = gen_moves(board, moves);
+        for (int i = 0; i < moves.size(); i++) {
+            std::cout << moves[i].to_string() << std::endl;
+        }
+
+        while (true) {
+            std::cout << "Enter move: ";
+            std::string move;
+            getline(std::cin, move);
+
+            int pos = -1;
+            for (int i = 0; i < moves.size(); i++) {
+                if (moves[i].to_string() == move) {
+                    pos = i;
+                    break;
+                }
+            }
+            if (pos != -1) {
+                std::cout << "Move: " << moves[pos].to_string() << std::endl;
+                board.make_move(moves[pos]);
+                break; 
+            }
+            std::cout << "Illegal move" << std::endl;
+        }
+
+        moves.clear();
+    }
     
-    U8 flags = 0;
-    Move move(e2, e4, flags);
-
-    board.make_move(move);
-
-    std::cout << board.to_string() << std::endl;
-
     return 0;
 }

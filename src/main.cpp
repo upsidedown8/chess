16cdb5092cffb47c079d5d74956ba89df49c6c64
Tom Thorogood <tomthorogood@outlook.com>
2021-02-02 02:40:58 +0000

    Updated main.cpp to use MoveList

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,15 @@ int main(int argc, char const *argv[]) {
     std::cout << "Enter FEN: ";
     getline(std::cin, fen);
     Board board(fen);
-    std::vector<Move> moves;
 
     while (true) {
         std::cout << "Board: " << std::endl;
         std::cout << board.to_string() << std::endl;
 
         std::cout << "Moves: " << std::endl;
-        U64 attacks = gen_moves(board, moves);
-        for (int i = 0; i < moves.size(); i++) {
-            std::cout << moves[i].to_string() << std::endl;
+        MoveList moveList = gen_moves(board);
+        for (int i = 0; i < moveList.size(); i++) {
+            std::cout << moveList.moves[i].to_string() << std::endl;
         }
 
         while (true) {
@@ -30,21 +29,19 @@ int main(int argc, char const *argv[]) {
             getline(std::cin, move);
 
             int pos = -1;
-            for (int i = 0; i < moves.size(); i++) {
-                if (moves[i].to_string() == move) {
+            for (int i = 0; i < moveList.size(); i++) {
+                if (moveList.moves[i].to_string() == move) {
                     pos = i;
                     break;
                 }
             }
             if (pos != -1) {
-                std::cout << "Move: " << moves[pos].to_string() << std::endl;
-                board.make_move(moves[pos]);
+                std::cout << "Move: " << moveList.moves[pos].to_string() << std::endl;
+                board.make_move(moveList.moves[pos]);
                 break; 
             }
             std::cout << "Illegal move" << std::endl;
         }
-
-        moves.clear();
     }
     
     return 0;
